773845d0870e94bd8c5d1038337316dd91ecb90f
Kyle Shainca Nicole V. Sambling <160310459+bling011@users.noreply.github.com>
2025-04-02 01:53:08 +0800

    Delete todos directory

--- todos/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Todo
-from .serializers import TodoSerializer
-
-class TodoViewSet(viewsets.ModelViewSet):  # Ensure you are using ModelViewSet
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
